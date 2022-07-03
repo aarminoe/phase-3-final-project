e9b59b799390b4677ece1c6529daddad9e2e907d
Aaron Noe <aarminoe@gmail.com>
2022-07-03 09:18:24 -0500

    added patch and delete functionality to tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,22 @@ class ApplicationController < Sinatra::Base
     task.to_json
   end
 
+  delete '/tasks' do
+    task = Task.find(params[:id])
+    task.destroy
+    task.to_json
+  end
+
+  patch '/tasks' do 
+    task = Task.find(params[:id])
+    task.update(
+      name: params[:name]
+      category: params[:category]
+      date: params[:date]
+      user_id: params[:user_id]
+    )
+  end
+
   get '/tasks' do
     task = Task.all
     task.to_json
