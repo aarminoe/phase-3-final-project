0de9d262f05d2e360be2c3cc3d824cb0e0672ebc
Aaron Noe <aarminoe@gmail.com>
2022-07-04 08:09:28 -0500

    got rid of timestamps on users

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2022_07_02_135717) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
