1bfa52a14a6a8166d5c2deb71834880ae43ea2aa
Aaron Noe <aarminoe@gmail.com>
2022-07-04 07:01:31 -0500

    reworked has_many and belongs_to on models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_204729) do
+ActiveRecord::Schema.define(version: 2022_07_02_135717) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "category"
     t.string "date"
+    t.integer "user_id"
+  end
+
+  create_table "updates", force: :cascade do |t|
+    t.string "comment"
     t.integer "user_id"
   end
 
