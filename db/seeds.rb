947007cb7a5734a97d468ef4c23d03438577ae4c
Aaron Noe <aarminoe@gmail.com>
2022-07-04 18:50:30 -0500

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 puts "🌱 Seeding spices..."
 
-User.create({username: "Aaron", password: "123"})
+User.create({username: "Aaron", password: "123", bio:'ok this is an application. Welcome to my application. Welcome to this application.'})
+User.create({username: 'mRose123', password: 'm293', bio: 'I like this application. It is useful'})
+User.create({username: 'Pizzaface', password: 'pizza', bio: 'This is interesting'})
+User.create({username: 'Loloo', password:'lol', bio: 'lololololololololololololololololololollolololol'})
+User.create({username: 'TheTaskHunter', password:'password', bio: 'Hunting Tasks and Going Crazy'})
+User.create({username: 'username', password: 'password', bio: 'bio'})
+
+
+
 Update.create({comment:"Almost done!", task_id: 1, user_id: 1})
 puts "✅ Done seeding!"
